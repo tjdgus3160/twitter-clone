7bec70b56c79df38b812ef93aec7afe312d64ae9
tjdgus3160 <tjdgus3160@gmail.com>
2021-02-03 17:02:33 +0900

    Realtime with Snapshot

--- src/components/Router.js
+++ src/components/Router.js
@@ -4,14 +4,14 @@ import Home from 'routes/Home';
 import Profile from 'routes/Profile';
 import Navigation from 'components/Navigation';
 
- const AppRouter = ({ isLoggedIn }) => {
+ const AppRouter = ({ isLoggedIn, userObj }) => {
     return (
         <Router>
             {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <>
-                        <Route exact path="/"><Home /></Route>
+                        <Route exact path="/"><Home userObj={userObj} /></Route>
                         <Route exact path="/profile"><Profile /></Route> 
                     </> 
                 ) : (
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,27 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
 
-const Home = () => {
+const Home = ({ userObj }) => {
     const [tweet, setTweet] = useState("");
     const [tweets, setTweets] = useState([]);
-    const getTweets = async() => {
-        const dbTweets = await dbService.collection("tweets").get();
-        dbTweets.forEach((document) => {
-            const tweetObject = {
-                ...document.data(),
-                id: document.id
-            };
-            setTweets((prev) => [tweetObject, ...prev]);
-        });
-    };
     useEffect(() => {
-        getTweets();
+        dbService.collection("tweets").onSnapshot((snapshot) => {
+            const tweetArray = snapshot.docs.map((doc) => ({
+                id: doc.id,
+                ...doc.data()
+            }));
+            setTweets(tweetArray);
+        });
     }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("tweets").add({
-            tweet,
-            createdAt: Date.now()
+            text: tweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid
         });
         setTweet("");
     };
@@ -38,7 +35,7 @@ const Home = () => {
             <div>
                 {tweets.map(tweet => (
                     <div key={tweet.id}>
-                        <h4>{tweet.tweet}</h4>
+                        <h4>{tweet.text}</h4>
                     </div>
                 ))}
             </div>
