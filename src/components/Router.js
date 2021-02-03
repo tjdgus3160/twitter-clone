4df3ade06e7a46fb7a2134d4453aaf5311dbde42
tjdgus3160 <tjdgus3160@gmail.com>
2021-02-03 15:56:09 +0900

    Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,21 +1,24 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import { HashRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
+import Profile from 'routes/Profile';
+import Navigation from 'components/Navigation';
 
  const AppRouter = ({ isLoggedIn }) => {
     return (
         <Router>
+            {isLoggedIn && <Navigation />}
             <Switch>
                 {isLoggedIn ? (
                     <>
-                        <Route exact path="/">
-                            <Home />
-                        </Route>
+                        <Route exact path="/"><Home /></Route>
+                        <Route exact path="/profile"><Profile /></Route> 
                     </> 
                 ) : (
-                    <Route exact path="/">
-                        <Auth />
-                    </Route>
+                    <>
+                        <Route exact path="/"><Auth /></Route>
+                        <Redirect from="*" to="/" />
+                    </>
                 )}
             </Switch>
         </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,16 @@
+import { authService } from 'fbase';
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
-export default () => <span>Profile</span>
+export default () => {
+    const history = useHistory();   // Hook을 사용한 redirection
+    const onLogOutClick = () => {
+        authService.signOut();
+        history.push("/");
+    }
+    return (
+        <>
+            <button onClick={onLogOutClick}>Log Out</button>
+        </>
+    );
+}
