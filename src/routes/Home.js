780196a54a82a785ef3546818eb65f4c83928a80
tjdgus3160 <tjdgus3160@gmail.com>
2021-02-03 16:46:43 +0900

    Read Tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { dbService } from 'fbase';
 
 const Home = () => {
     const [tweet, setTweet] = useState("");
+    const [tweets, setTweets] = useState([]);
+    const getTweets = async() => {
+        const dbTweets = await dbService.collection("tweets").get();
+        dbTweets.forEach((document) => {
+            const tweetObject = {
+                ...document.data(),
+                id: document.id
+            };
+            setTweets((prev) => [tweetObject, ...prev]);
+        });
+    };
+    useEffect(() => {
+        getTweets();
+    }, []);
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("tweets").add({
@@ -17,8 +31,17 @@ const Home = () => {
     };
     return (
         <div>
-            <input type="text" placeholder="What's on your mind?" value={tweet} onChange={onChange} maxLength={120} />
-            <input type="submit" onClick={onSubmit} value="Tweet" />
+            <form onSubmit={onSubmit}>
+                <input type="text" placeholder="What's on your mind?" value={tweet} onChange={onChange} maxLength={120} />
+                <input type="submit" value="Tweet" />
+            </form>
+            <div>
+                {tweets.map(tweet => (
+                    <div key={tweet.id}>
+                        <h4>{tweet.tweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
